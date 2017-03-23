deca08aeb37235dfe8f34388673d04f6ab71af5f
johann <johann.laulik@ttu.ee>
2017-03-23 15:29:16 +0200

    23.03

--- moon_zoom.js
+++ moon_zoom.js
@@ -1,4 +1,11 @@
 /*jshint browser: true, jquery: true*/
 $(document).on('click','.moonTxt',function() {
     $('body').addClass('activated');
+    //var w = $('body').removeClass('activated').addClass('works');
+    setTimeout( function(){
+        $('body').removeClass('activated').addClass('working');
+    }, 2000);
+     setTimeout( function(){
+        $('body').addClass('done');
+    }, 6000);
 });
